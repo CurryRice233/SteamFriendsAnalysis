bf3724810942572eb51bf8e86d80b7edb79c8f30
CurryRice233 <nmeia@qq.com>
2019-12-01 18:31:37 +0100

    with cookie

--- main.py
+++ main.py
@@ -5,23 +5,43 @@ from bs4 import BeautifulSoup
 from player import Player
 
 
+url = 'https://steamcommunity.com/profiles/76561198158810211/friends/'  # Your profiles
+cookies = {'steamLoginSecure': 'Your Cookie'}
+
+
 def get_games(profile_link):
-    bs = BeautifulSoup(requests.get(profile_link).text, "lxml")
+    bs = BeautifulSoup(requests.get(profile_link, cookies=cookies).text, "lxml")
     pattern = re.compile(r"var rgGames = (.*);")
     script = bs.find('script', text=pattern)
-    match = pattern.search(script.text)
-    return json.loads(match.group(1))
+    if script is None:
+        return None
+    else:
+        match = pattern.search(script.text)
+        return json.loads(match.group(1))
 
 
-url = 'https://steamcommunity.com/profiles/76561198158810211/friends/'  # 需要爬数据的网址
-page = requests.get(url)
+page = requests.get(url, cookies=cookies)
 soup = BeautifulSoup(page.text, "lxml")
 names = soup.find_all('div', class_='friend_block_content')
 profiles = soup.find_all('a', class_='selectable_overlay')
 
 friends = []
 for i in range(len(names)):
-    friends.insert(i, Player(names[i].contents[0], profiles[i].get('href'), get_games(profiles[i].get('href'))))
+    link = profiles[i].get('href')
+    player = Player(names[i].contents[0], link, get_games(link + "/games/?tab=all"))
+    friends.insert(i, player)
+    info = "\nFriend: " + player.name
+    if player.games is not None and len(player.games) > 0:
+        info += "\nGame: " + player.games[0]['name']
+        if "hours_forever" in player.games[0]:
+            info += "\tPlayed: " + player.games[0]['hours_forever']+"h"
+        else:
+            info += "\tPlayed: no data"
+    else:
+        info += "\nNo game found!"
+    print(info)
 
+'''
 for friend in friends:
-    print("\nname: " + friend.name + "\tlink: " + friend.link)
+    print("\nname: " + friend.name + "\tlink: " + friend.link + "\tgame: " + friend.games[0].name)
+'''
--- test.py
+++ test.py
@@ -3,11 +3,25 @@ import re
 import json
 from bs4 import BeautifulSoup
 
-url = 'https://steamcommunity.com/profiles/76561198158810211/games/?tab=all'  # 需要爬数据的网址
-soup = BeautifulSoup(requests.get(url).text, "lxml")
 
-pattern = re.compile(r"var rgGames = (.*);")
-script = soup.find('script', text=pattern)
-match = pattern.search(script.text)
-games = json.loads(match.group(1))
-print(games[0]['appid'])
+cookies = {'steamLoginSecure': 'Your Cookie'}
+
+url = 'https://steamcommunity.com/profiles/76561198158487803/games/?tab=all'
+soup = BeautifulSoup(requests.get(url, cookies=cookies).text, "lxml")
+print(soup)
+
+'''
+{"appid": 578080, "name": "PLAYERUNKNOWN'S BATTLEGROUNDS",
+ "logo": "https:\/\/steamcdn-a.akamaihd.net\/steam\/apps\/578080\/capsule_184x69.jpg", "friendlyURL": 578080,
+ "availStatLinks": {"achievements": true, "global_achievements": true, "stats": false, "gcpd": false,
+                    "leaderboards": false, "global_leaderboards": false}, "hours_forever": "338",
+ "last_played": 1573638854}
+'''
+
+'''
+{"appid": 251570, "name": "7 Days to Die",
+ "logo": "https:\/\/steamcdn-a.akamaihd.net\/steam\/apps\/251570\/capsule_184x69.jpg", "friendlyURL": 251570,
+ "availStatLinks": {"achievements": true, "global_achievements": true, "stats": false, "gcpd": false,
+                    "leaderboards": false, "global_leaderboards": false}}
+'''
+
